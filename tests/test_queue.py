7849c16e904b135d822601fe82e28929cc957d5d
Matthias Probst <matth.probst@gmail.com>
2022-11-03 16:43:46 +0100

    solution to pass an argument individual for each task

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,3 +1,4 @@
+import pathlib
 import random
 import unittest
 from typing import Dict, Tuple, Union
@@ -5,9 +6,11 @@ from typing import Dict, Tuple, Union
 from pydqueue import Task, Queue
 
 
+# simulation function that is the same for all tasks
 def run_simulation(parents_success: Union[bool, None], input_data: Dict,
                    verbose: bool = False) -> Tuple[bool, Dict]:
     """simulate a simulation. A random variable decides if simulation failes or not"""
+
     if 'result' in input_data:
         if not parents_success:
             input_data['result'] = 0
@@ -29,22 +32,62 @@ def run_simulation(parents_success: Union[bool, None], input_data: Dict,
     return True, {'result': 1}
 
 
+# if every task needs additional input, this can be solved using a class
+# then an instance of that class is passed to each task
+class Simulation:
+    """Object used to create instances for each task"""
+
+    def __init__(self, simulation_filename: pathlib.Path):
+        self.simulation_filename = simulation_filename
+
+    def __call__(self, parents_success: Union[bool, None], input_data: Dict,
+                 verbose: bool = False):
+        return self.run_simulation(parents_success,
+                                   input_data,
+                                   verbose)
+
+    def run_simulation(self, parents_success: Union[bool, None], input_data: Dict,
+                       verbose: bool = False) -> Tuple[bool, Dict]:
+        """simulate a simulation. A random variable decides if simulation failes or not"""
+        print(f'filename: {self.simulation_filename}')
+
+        if 'result' in input_data:
+            if not parents_success:
+                input_data['result'] = 0
+            if random.random() < 0.5:
+                # make simulation fail
+                if verbose:
+                    print('Simulation failed')
+                return False, {'result': input_data['result']}
+            if verbose:
+                print('Simulation succeeded')
+            return True, {'result': input_data['result'] + 1}
+        if random.random() < 0.5:
+            # make simulation fail
+            if verbose:
+                print('Simulation failed')
+            return False, {'result': 0}
+        if verbose:
+            print('Simulation succeeded')
+        return True, {'result': 1}
+
+
 class TestQueue(unittest.TestCase):
 
     def test_queue(self):
         # A has no parent, because it is the initial task
-        A = Task(run_simulation, name='Init')
+        A = Task(Simulation('one'), name='Init')
 
-        B = Task(run_simulation)
+        B = Task(Simulation('two'))
         B.add_parent(A)
 
-        C = Task(run_simulation, name='C')
+        C = Task(Simulation('two'), name='C')
         C.add_parents(B, A)
 
-        D = Task(run_simulation, name='D')
+        D = Task(Simulation('3'), name='D')
         D.add_parents(A)
 
-        E = Task(run_simulation, name='E')
+        E = Task(Simulation('4'), name='E')
         E.add_parents(D, A)
 
         with self.assertRaises(RuntimeError):
